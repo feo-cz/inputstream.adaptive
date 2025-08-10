83aeae2462470b27d517a74ec393f06b8879b583
Craig Carnell <1188869+cscd98@users.noreply.github.com>
2025-08-10 10:56:31 +0100

    webOS: allow secure audio

--- src/decrypters/widevine/WVDecrypter.h
+++ src/decrypters/widevine/WVDecrypter.h
@@ -50,8 +50,12 @@ public:
   virtual void ReleaseBuffer(void* instance, void* buffer);
   virtual std::string_view GetLibraryPath() const override { return m_libraryPath; }
 
+#ifdef TARGET_WEBOS
+  virtual bool IsSecureDecoderAudioSupported() override { return true; }
+#else
   //! @todo: Secure path for audio is not implemented
   virtual bool IsSecureDecoderAudioSupported() override { return false; }
+#endif
 
 private:
   std::shared_ptr<CWVCdmAdapter> m_WVCdmAdapter;
